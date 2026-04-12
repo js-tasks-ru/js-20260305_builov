fc88d22c6c8edce92c0220137ffce8197e0452e0
Dmitriy <builov@inbox.ru>
2026-04-12 18:43:11 +0300

    task 6-1 edit

--- 06-events-practice/1-sortable-table-v2/index.ts
+++ 06-events-practice/1-sortable-table-v2/index.ts
@@ -28,9 +28,9 @@ export default class SortableTable {
   private tableElement: HTMLElement | null;
 
   private headersConfig: SortableTableHeader[];
-  private data: SortableTableData[];
+  private data: SortableTableData[] | undefined;
   private sorted: SortableTableSort | undefined;
-  private isSortLocally: boolean;
+  private isSortLocally: boolean | undefined;
 
   constructor(headersConfig: SortableTableHeader[] = [], {
     data = [],
@@ -43,30 +43,28 @@ export default class SortableTable {
     this.isSortLocally = isSortLocally;
     this.tableElement = this.buildTableElement();
 
-    this.tableElement.querySelectorAll('[data-element="header"] > .sortable-table__cell > span').forEach(element => {
-      element.addEventListener('click', e => {
+    this.tableElement.querySelector('[data-element="header"]').addEventListener('click', e => {
+        const element =  e.target as HTMLElement;
+        const dataset = element.closest(".sortable-table__cell").dataset;
 
-        console.log(e.target);
+        const id = dataset.id
+        const order = (dataset.order === 'desc') ? 'asc' : 'desc';
+
+        // console.log(id, order);
+
+        this.sorted = {
+          id: id,
+          order: order
+        }
+
+        this.sort();
       });
-    });
   }
 
 
   private buildTableElement(): HTMLElement {
-    const header = this.headersConfig
-      .map(header => `
-                 <div class="sortable-table__cell" data-id="${header.id}" data-sortable="${header.sortable}">
-                    <span>${header.title}</span>
-                 </div>
-      `).join('');
-
-    const body = this.data.map(item => {
-      const cells = this.headersConfig.map(header => {
-        const value = item[header.id];
-        return header.template ? header.template(value) : `<div class="sortable-table__cell">${value}</div>`;
-      }).join('');
-      return `<a href="/products/${item.id}" class="sortable-table__row">${cells}</a>`;
-    }).join('');
+    const header = this.buildTableHeader();
+    const body = this.buildTableBody();
 
     const html = `
     <div data-element="productsContainer" class="products-list__container">
@@ -84,6 +82,40 @@ export default class SortableTable {
   }
 
 
+  private buildTableHeader() {
+    return this.headersConfig
+      .map(header => {
+        let arrow = '';
+        if (this.sorted?.id === header.id) {
+          arrow = `<span data-element="arrow" class="sortable-table__sort-arrow">
+                      <span class="sort-arrow"></span>
+                    </span>`;
+        }
+
+        return `<div class="sortable-table__cell" data-id="${header.id}" data-sortable="${header.sortable}" data-order="${this.sorted?.order}">
+                    <span>${header.title}</span>
+                    ${arrow}
+                 </div>`
+      }).join('');
+  }
+
+
+  private buildTableBody() {
+    let body = '';
+    if (this.data) {
+      body = this.data.map(item => {
+        const cells = this.headersConfig.map(header => {
+          const value = item[header.id];
+          return header.template ? header.template(value) : `<div class="sortable-table__cell">${value}</div>`;
+        }).join('');
+        return `<a href="/products/${item.id}" class="sortable-table__row">${cells}</a>`;
+      }).join('');
+    }
+
+    return body;
+  }
+
+
   public get element() {
     return this.tableElement;
   }
@@ -98,10 +130,68 @@ export default class SortableTable {
   }
 
   private sortOnClient() {
+    const sortBy = this.headersConfig.find(h => h.id === this.sorted?.id);
+    const sortType = sortBy?.sortType || 'string';
 
+    if (this.data) {
+      this.data.sort((a, b) => {
+        const aVal = a[this.sorted?.id];
+        let bVal = b[this.sorted?.id];
+
+        if (aVal === undefined || aVal === null) {
+          return 1;
+        }
+        if (bVal === undefined || bVal === null) {
+          return -1;
+        }
+
+        let result = 0;
+        if (sortType === 'number') {
+          result = (aVal as number) - (bVal as number);
+        } else {
+          result = String(aVal).localeCompare(String(bVal), ['ru', 'en']);
+        }
+
+        return this.sorted?.order === 'asc' ? result : -result;
+      });
+    }
+
+    this.updateTable();
   }
 
   private sortOnServer() {
 
+  }
+
+  private updateTable() {
+    if (!this.tableElement) {
+      return;
+    }
+
+    const header = this.buildTableHeader();
+    const body = this.buildTableBody();
+
+    // console.log(header);
+
+    if ("querySelector" in this.tableElement) {
+      this.tableElement.querySelector(".sortable-table__header").innerHTML = header;
+    }
+
+    if ("querySelector" in this.tableElement) {
+      this.tableElement.querySelector(".sortable-table__body").innerHTML = body;
+    }
+
+  }
+
+  public remove() {
+    if (this.tableElement) {
+      this.tableElement.remove();
+    }
+  }
+
+  public destroy() {
+    this.remove();
+
+    this.tableElement = null;
   }
 }
